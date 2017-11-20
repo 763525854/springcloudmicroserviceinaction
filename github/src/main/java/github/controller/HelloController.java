3e67fa7922925ba948bf4100b8a5d897e088c7c0
rhyme <763525854@qq.com>
2017-11-20 13:50:49 +0800

    修改请求控制类

--- github/src/main/java/github/controller/HelloController.java
+++ github/src/main/java/github/controller/HelloController.java
@@ -1,8 +1,5 @@
 package github.controller;
 
-import java.io.FileNotFoundException;
-import java.lang.reflect.Method;
-
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
