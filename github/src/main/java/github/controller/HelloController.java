861e26a29215d593601388288075c8200e56e653
17151 <weiping.gong@hand-china.com>
2017-11-24 10:03:05 +0800

    k

--- github/src/main/java/github/controller/HelloController.java
+++ github/src/main/java/github/controller/HelloController.java
@@ -1,13 +1,21 @@
 package github.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 @RestController
 @RequestMapping("/hello")
 public class HelloController {
+	
+	@Autowired
+	RestTemplate restTemplate;
+	
 	@RequestMapping("/index")
 	public String index() throws Exception {
+		ResponseEntity<String> responseEntity=restTemplate.getForEntity(null, null);
 		return "index";
 
 	}
