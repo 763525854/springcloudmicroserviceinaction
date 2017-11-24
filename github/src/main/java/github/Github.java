2191d6bdcc7144b12893d6d7cbc902e4dc231034
17151 <weiping.gong@hand-china.com>
2017-11-24 10:02:56 +0800

    h

--- github/src/main/java/github/Github.java
+++ github/src/main/java/github/Github.java
@@ -2,9 +2,18 @@ package github;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication(scanBasePackages = { "github" })
 public class Github {
+	@Bean
+	@LoadBalanced
+	RestTemplate restTemplate() {
+		return new RestTemplate();
+	}
+
 	public static void main(String[] args) {
 		SpringApplication.run(Github.class, args);
 	}
